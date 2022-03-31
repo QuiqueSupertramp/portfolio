f078cde6996be22b64ccc82dc7ced616eaace69a
Quique Supertramp <quiquesupertramp@gmail.com>
2022-03-31 16:08:50 +0200

    font poppins

--- src/components/SoftSkills/SoftSkillsData.js
+++ src/components/SoftSkills/SoftSkillsData.js
@@ -9,32 +9,32 @@ const data = [
 	{
 		img: teamwork,
 		title: 'Trabajo en equipo',
-		desc: 'Llevo años creando sinergias, poniendo el foco en la responsabilidad individual y proyectando hacia el colectivo. La herramienta clave es la asertividad.',
+		desc: '"Llevo años creando sinergias, poniendo el foco en la responsabilidad individual y proyectando hacia el colectivo. La herramienta clave es la asertividad."',
 	},
 	{
 		img: pressure,
 		title: 'Tolerancia a la presión',
-		desc: 'Acostumbrado a trabajar bajo presión, tanto en el día a día como en la toma de decisiones. La herramiento clave es la preparación.',
+		desc: '"Acostumbrado a trabajar bajo presión, tanto en el día a día como en la toma de decisiones. La herramiento clave es la preparación."',
 	},
 	{
 		img: changes,
 		title: 'Adaptación al cambio',
-		desc: 'Vengo de un mundo dónde el primero en adaptarse juega con ventaja. Las herramientas clave son la curiosidad y poner el foco en los detalles',
+		desc: '"Vengo de un mundo dónde el primero en adaptarse juega con ventaja. Las herramientas clave son la curiosidad y poner el foco en los detalles."',
 	},
 	{
 		img: conflicts,
 		title: 'Resolución de conflictos',
-		desc: 'Son parte del día a día y la semilla del crecimiento de un equipo. Es clave escuchar, buena comunicación y cierta creatividad.',
+		desc: '"Son parte del día a día y la semilla del crecimiento de un equipo. Es clave escuchar, buena comunicación y cierta creatividad."',
 	},
 	{
 		img: results,
 		title: 'Orientado a resultados',
-		desc: 'Estoy acostumbrado a trabajar con objetivos a corto y largo plazo. Serenidad y una buena planificación son los ingredientes del éxito.',
+		desc: '"Estoy acostumbrado a trabajar con objetivos a corto y largo plazo. Serenidad y una buena planificación son los ingredientes del éxito."',
 	},
 	{
 		img: knowledge,
 		title: 'Formación continuada',
-		desc: 'Conozco el camino y los métodos de aprendizaje eficientes, lo que me permite ser autodidacta y tener facilidad para formar a otras personas.',
+		desc: '"Conozco el camino y los métodos de aprendizaje eficientes, lo que me permite ser autodidacta y tener facilidad para formar a otras personas."',
 	},
 ];
 
