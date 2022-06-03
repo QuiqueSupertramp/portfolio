87f17b5b58eb84e2aa49b5c27986599e02d32ece
Quique Supertramp <quiquesupertramp@gmail.com>
2022-06-03 17:55:54 +0200

    fix download cv

--- src/components/Header/components/Sidebar.jsx
+++ src/components/Header/components/Sidebar.jsx
@@ -2,21 +2,21 @@ import './Sidebar.css';
 
 const Sidebar = () => {
 	return (
-		<div className='header__text'>
-			<span className='header__span'>const NAME = </span>
+		<div className="header__text">
+			<span className="header__span">const NAME = </span>
 			<h1>Quique Supertramp</h1>
 			<h2>
-				<span className='orange'>{'<'}</span>DesarrolladorWeb
-				<span className='orange'>{'/>'}</span>
+				<span className="orange">{'<'}</span>DesarrolladorWeb
+				<span className="orange">{'/>'}</span>
 			</h2>
-			<div className='header__buttons'>
-				<a href='#about' className='btn btn--filled'>
+			<div className="header__buttons">
+				<a href="#about" className="btn btn--filled">
 					About Me
 				</a>
 				<a
-					download='CV Enrique López'
-					href='public/resources/QuiqueLopezCV.pdf'
-					className='btn btn--ghost'>
+					download="CV Enrique López"
+					href="resources/QuiqueLopezCV.pdf"
+					className="btn btn--ghost">
 					Download CV
 				</a>
 			</div>
