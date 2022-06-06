6e431223235966c75efb870a70f79ebe53491560
Quique Supertramp <quiquesupertramp@gmail.com>
2022-06-06 20:15:46 +0200

    fix title images

--- src/components/Header/components/AvatarMenu.jsx
+++ src/components/Header/components/AvatarMenu.jsx
@@ -10,21 +10,29 @@ const AvatarMenu = () => {
 					className="header__social"
 					target="_blank"
 					rel="noopener noreferrer">
-					<img src="images/social/github.svg" alt="github" />
+					<img src="images/social/github.svg" alt="github" title="github" />
 				</a>
 				<a
 					href="https://linkedin.com/in/quiquesupertramp"
 					className="header__social header__social1"
 					target="_blank"
 					rel="noopener noreferrer">
-					<img src="images/social/linkedin.svg" alt="linkedin" />
+					<img
+						src="images/social/linkedin.svg"
+						alt="linkedin"
+						title="linkedin"
+					/>
 				</a>
 				<a
 					href="https://instagram.com/quiquesupertramp"
 					className="header__social header__social2"
 					target="_blank"
 					rel="noopener noreferrer">
-					<img src="images/social/instagram.svg" alt="instagram" />
+					<img
+						src="images/social/instagram.svg"
+						alt="instagram"
+						title="instagram"
+					/>
 				</a>
 			</div>
 			<img
